af2f14a14c7d620d3b76a31b7ff9a02537657dd5
Ali731-Amb <12252@holbertonstudents.com>
2025-10-27 10:52:39 +0100

    Task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
+ * _isupper- checks for alphabetic character
  * @c: the character to check
- *
  * Return: 1 if c is a letter (uppercase), 0 otherwise
  */
 
