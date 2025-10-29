331c04fc52cf050d20eed1f955d9f4ac5970ea96
Ali731-Amb <12252@holbertonstudents.com>
2025-10-29 10:39:28 +0100

    Task 03

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - affiche une chaîne suivie d'une nouvelle ligne
+ * @str: pointeur vers la chaîne à afficher
+ *
+ * Return: rien (void)
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
