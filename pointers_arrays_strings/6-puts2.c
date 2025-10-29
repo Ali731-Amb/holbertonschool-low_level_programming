45dc75f7e23ac3d87b90efbb3490236600fbf207
Ali731-Amb <12252@holbertonstudents.com>
2025-10-29 21:39:58 +0100

    Task 06

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -13,13 +13,14 @@ void puts2(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
+			i++;
 
 	{
 		if (i % 2 == 0)
 		{
-			printf("%c",str[i]);
+			printf("%c", str[i]);
 		}
-			i++;
+
 	}
 
 	_putchar('\n');
