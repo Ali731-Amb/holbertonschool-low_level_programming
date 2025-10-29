50a850c9854da7b2c082f77698a48266e92bf795
Ali731-Amb <12252@holbertonstudents.com>
2025-10-29 21:35:12 +0100

    Task 06

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - affiche un caractére sur 2 d'une string
+ * @str: pointeur vers la chaine
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+
+	{
+		if (i % 2 == 0)
+		{
+			printf("%c",str[i]);
+		}
+			i++;
+	}
+
+	_putchar('\n');
+}
