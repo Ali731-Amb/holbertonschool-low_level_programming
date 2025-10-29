faca499db24725ac8c617a08bc7aa1bcbb03fec5
Ali731-Amb <12252@holbertonstudents.com>
2025-10-29 10:04:32 +0100

    Task 02

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the
+ * length of a string.
+ *
+ *@s: pointeur vers la chaîne de caractères à mesurer
+ *
+ *Return: count
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	{
+		while (s[count] != '\0')
+			count++;
+	}
+	return (count);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+int _strlen(char *s);
 
 #endif
