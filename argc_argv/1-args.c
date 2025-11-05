de9b3d0f558ae6b4a7ae5d5f718c06e95a52c59e
Ali731-Amb <12252@holbertonstudents.com>
2025-11-05 16:31:45 +0100

    Task 01

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée principal du programme.
+ * Cette fonction imprime le nom par lequel elle a été exécutée,
+ * suivi d'un saut de ligne.
+ *
+ * @argc: Le nombre d'arguments sur la ligne de commande (non utilisé).
+ * @argv: Un tableau de pointeurs vers les arguments de la ligne de commande.
+ * argv[0] contient le nom du programme.
+ *
+ * Return: Toujours 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
