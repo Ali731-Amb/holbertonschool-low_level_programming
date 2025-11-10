2f51603724e4b360ba5e660f9793d6fb6708e102
Ali731-Amb <12252@holbertonstudents.com>
2025-11-11 00:16:42 +0100

    Task 02

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
+#include <string.h>
+#include "main.h"
 
 /**
  * str_concat - Concatène deux chaînes de caractères
@@ -15,29 +16,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	size_t i = 0;
 	size_t len1 = 0;
 	size_t len2 = 0;
 	char *result;
-	{
-		if (s1)
-			while (len1[s1])
-				s1++;
-	}
-	{
-		if (s2)
-			while (len2[s2])
-				s2++;
-	}
-	result = malloc(len1 + len2 + 1);
+	size_t i;
 
-	{
-		if (s1)
-			for (i = 0; i < len1; i++)
-				result[i] = s1[i];
-	}
-	for (i = 0; i < len2; i++)
-		result[len1 + i] = s2[i];
+	if (s1)
+		for (len1 = 0; s1[len1]; len1++)
+			;
+
+	if (s2)
+		for (len2 = 0; s2[len2]; len2++)
+			;
+
+	result = malloc(len1 + len2 + 1);
+	if (result == NULL)
+		return (NULL);
+
+	if (s1)
+		for (i = 0; i < len1; i++)
+			result[i] = s1[i];
+
+	if (s2)
+		for (i = 0; i < len2; i++)
+			result[len1 + i] = s2[i];
 
 	result[len1 + len2] = '\0';
 
