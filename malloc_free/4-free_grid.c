240f3d3b066c5210fcfbb4570d2787eeb159a899
Ali731-Amb <12252@holbertonstudents.com>
2025-11-11 12:54:51 +0100

    Task 04

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - function that frees a 2
+ * dimensional grid previously created by your alloc_grid function.
+ *
+ *@grid: Tableau
+ *@height: Hauteur du tableau
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
