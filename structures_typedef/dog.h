65ee5279d098512878892acb32f90911bafd1fc6
Ali731-Amb <12252@holbertonstudents.com>
2025-11-12 11:57:01 +0100

    Projet 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#define DOG_H
+#ifdef DOG_H
 #include <stdio.h>
 
 struct dog
 {
-*char names;
+char* name;
 float age;
-*char owner;
+char* owner;
 };
 
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-
-
-#ifdef
+#endif
