34d65649e5fae98cb2a6db647de1813eb78d5984
Ali731-Amb <12252@holbertonstudents.com>
2025-11-23 21:22:33 +0100

    Task 01

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ *print_numbers - function that prints numbers, followed by a new line.
+ *@separator: La virgule
+ *@n: Nombre d'un argument
+ *
+ *Return: Nombre à afficher avec la virgule.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+	int nombre;
+
+	va_start(arg, n);
+
+	if (separator == 0)
+		return;
+
+			for (i = 0; i < n; i++)
+		{
+			nombre = va_arg(arg, unsigned int);
+			printf("%d", nombre);
+			if (i < n - 1 && separator != NULL)
+				printf("%s", separator);
+		}
+
+	printf("\n");
+	va_end(arg);
+}
