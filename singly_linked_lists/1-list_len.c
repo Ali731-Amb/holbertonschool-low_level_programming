53b34e30eddf83426d4e688a69db292e8df3b806
Ali731-Amb <12252@holbertonstudents.com>
2025-12-01 17:05:46 +0100

    Task

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *list_len - function that returns the
+ *number of elements in a linked list_t list.
+ *
+ *@h: Head
+ *Return: Count
+ */
+
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+	if (h->str == NULL)
+	printf("[0] (nil)\n");
+
+	else
+	printf("%ld", count);
+	count++;
+	h = h->next;
+
+}
+return(count);
+
+}
