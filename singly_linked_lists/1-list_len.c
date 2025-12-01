6415a4df943c29eac22965878134ebbaae9be671
Ali731-Amb <12252@holbertonstudents.com>
2025-12-01 17:10:07 +0100

    Task

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -15,14 +15,13 @@ size_t count = 0;
 while (h != NULL)
 {
 	if (h->str == NULL)
-	printf("[0] (nil)\n");
+	return(-1);
 
 	else
-	printf("%ld", count);
 	count++;
 	h = h->next;
 
 }
-return(count);
+return (count);
 
 }
