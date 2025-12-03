9cd7f6e9696f9a0b06d572039de8c6ea6396809d
Ali731-Amb <12252@holbertonstudents.com>
2025-12-03 21:10:24 +0100

    Task

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *temp;
+	dlistint_t *temp = *h;
 	unsigned int i = 0;
 
 	if (h == NULL)
