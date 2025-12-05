4ef1d55b28c4f0e09518f2ac644b9ef8af4770a1
Ali731-Amb <12252@holbertonstudents.com>
2025-12-05 23:39:07 +0100

    Task

---
+++ file_io/3-cp.c
@@ -0,0 +1,65 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: The array of command-line arguments (file names).
+ * Return: 0 on success, or an exit code on failure (97-100).
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	char *buffer;
+	ssize_t r_bytes;
+	ssize_t w_bytes;
+
+	if (argc != 3)
+	{
+		dprintf(2, "usage: cp file_from_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	buffer = malloc(1024);
+	while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
+	{
+		w_bytes = write(fd_to, buffer, r_bytes);
+
+		if (w_bytes != r_bytes)
+		{
+			dprintf(2, "Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (r_bytes == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd%d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
