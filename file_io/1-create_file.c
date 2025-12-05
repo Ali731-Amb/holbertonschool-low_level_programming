be3c448f5aa2dec80e1205225af155ebfb1d51ef
Ali731-Amb <12252@holbertonstudents.com>
2025-12-05 19:38:31 +0100

    Task

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *create_file - Create a function that creates a file
+ *
+ *@filename: Name of file.
+ *@text_content: charactere string
+ *Return: -1 or 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
